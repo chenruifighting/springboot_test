b70645fc3d1e73ecbc23d6b41fde5734b4bbbba4
陈睿 <chenruifighting@163.com>
2020-03-13 00:32:46 +0800

    在controller中添加redis缓存

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -3,10 +3,10 @@ package com.example.demo.controller;
 import com.example.demo.pojo.Account;
 import com.example.demo.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -18,9 +18,18 @@ import java.util.List;
 public class AccountController {
     @Autowired
     private AccountService accountService;
+    @Autowired
+    private RedisTemplate redisTemplate;
     @GetMapping("findAll")
-    public String findAll(Model model){
-        List<Account> accounts=accountService.findAll();
+    public String findAll(Model model) {
+        List<Account> accounts=redisTemplate.opsForList().range("findAll",0,-1);
+        if(accounts.isEmpty()){
+            accounts=accountService.findAll();
+            redisTemplate.opsForList().leftPushAll("findAll",accounts);
+            System.out.println("从数据库中获取");
+        }else {
+            System.out.println("从缓存中获取");
+        }
         model.addAttribute("accounts",accounts);
         return "Account";
     }
--- src/main/java/com/example/demo/pojo/Account.java
+++ src/main/java/com/example/demo/pojo/Account.java
@@ -2,12 +2,14 @@ package com.example.demo.pojo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author 陈睿
  * @date 2020-03-11 14:33
  */
 @Data
-public class Account {
+public class Account implements Serializable {
     private Integer id;
     private String name;
     private String money;
