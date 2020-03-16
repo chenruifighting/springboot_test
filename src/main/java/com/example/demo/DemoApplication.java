dd7a2cc5424906ef4a7878bc3789072d760b8783
陈睿 <chenruifighting@163.com>
2020-03-16 20:53:05 +0800

    添加redis缓存内容，还要再结合spring基于redis的注解使用

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching //开启缓存
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.mapper.AccountMapper;
+import com.example.demo.pojo.Account;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+    @Autowired
+    private AccountMapper accountMapper;
+    @Test()
+    public void test(){
+        List<Account> accounts=accountMapper.findAll();
+        System.out.println(accounts);
     }
 
 }
